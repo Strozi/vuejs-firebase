55d3cba1f3b3e3cc9139cf6fb29db13f314ac308
Michał Stróżyński <michal.strozynski@o2.pl>
2017-08-19 11:41:50 +0200

    Adding adding and deleting functionality and toast

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,111 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue.js 2 & Firebase Sample Application</h1>
+    </div>
+
+    <div class="panel panle-default">
+        <div class="panel-heading">
+          <h3>Add Book </h3>
+        </div>
+        <div class="panel-body">
+          <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+            <div class="form-group">
+              <label for="bookTitle">Title:</label>
+              <input type="text" id="bookTitle" class="form-control" v-model="newBook.title"/>
+            </div>
+            <div class="form-group">
+              <label for="bookAuthor">Author:</label>
+              <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author"/>
+            </div>
+            <div class="form-group">
+              <label for="bookUrl">Url:</label>
+              <input type="text" id="bookUrl" class="form-control" v-model="newBook.url"/>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Add Book" />
+          </form>
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panle-heading">
+        <h3>Books Lists</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a target="_blank" v-bind:href="book.url">{{book.title}}</a>
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+              <td>
+                <span class="glyphicon glyphicon-trash" v-on:click="removeBook(book)"></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+
+import Firebase from 'firebase'
+import toastr from 'toastr'
+let config = {
+    // from your firebase account 
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+
+};
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+let booksRef = db.ref('books');
 
 export default {
   name: 'app',
-  components: {
-    Hello
+  firebase: {
+    books: booksRef
+  },
+  data (){
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url:''
+      }
+    }
+  },
+  methods:{
+    addBook: function(){
+      booksRef.push(this.newBook);
+      this.newBook.title='';
+      this.newBook.author='';
+      this.newBook.url='';
+    },
+    removeBook: function(book){
+      booksRef.child(book['.key']).remove();
+      toastr.success("Book removed");
+    }
   }
 }
 </script>
@@ -21,7 +115,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
